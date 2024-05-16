33c54e139778612fa5f8d6e723766e8996baecaa
CrystalHosting1 <163294945+CrystalHosting1@users.noreply.github.com>
2024-05-16 11:39:46 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ sudo apt-get install -y ssh
 sudo apt-get install -y openssh-server
 sudo apt-get install -y systemd
 sudo apt-get install -y systemctl
+sudo apt-get install -y docker.ok 
 sudo curl ifconfig.me
-sudo ufw allow mysql && sudo ufw allow OpenSSH && sudo ufw allow ssh && sudo ufw allow 80 && sudo ufw 443 && sudo ufw allow 443 && sudo ufw 8080
+sudo ufw allow mysql && sudo ufw allow OpenSSH && sudo ufw allow ssh && sudo ufw allow 80 && sudo ufw 443 && sudo ufw allow 443 && sudo ufw 8080 allow
 passwd root 
-sshd 
 service ssh start 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
-sudo apt install -y playit
+sudo apt install playit
 bash <(curl -s https://pterodactyl-installer.se/)
